0f138650b9add98f0da9d2d45453af3c6c2400f1
Jack Treado <john.treado@yale.edu>
2021-07-09 11:57:56 -0400

    added processing code:

---
+++ bash/process_sphereGel.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# directories with code
+maindir=~/flowers/phasesep
+
+# directory for all output for cell simulations
+outputdir=/gpfs/project/fas/ohern/jdt45/flowers
+
+# directory for simulations specific to sphereGel
+simtypedir=$outputdir/sphereGel
+
+# directory to save matfiles
+savedir=$simtypedir/matfiles
+
+# make directories, unless they already exist
+mkdir -p $outputdir
+mkdir -p $simtypedir
+mkdir -p $savedir
+mkdir -p bin
+mkdir -p tasks
+mkdir -p slurm
+mkdir -p out
+
+# inputs
+N=$1
+dr=$2
+dphi=$3
+dlz=$4
+l2=$5
+partition=$6
+time=$7
+startSeed="${8}"
+endSeed="${9}"
+
+# name strings
+basestr=sgel_N"$N"_dr"$dr"_dphi"$dphi"_dlz"$dlz"_l2"$l2"
+runstr="$basestr"_startseed"$startSeed"_endseed"$endSeed"_PROCESS
+
+# access directory specific for this simulation
+simdatadir=$simtypedir/$basestr
+if [[ ! -d $simdatadir ]]
+then
+    echo -- sim directory "$simdatadir" does not exist, ending.
+    exit 1
+fi
+
+# create task file
+taskf=tasks/"$runstr".task
+rm -f $taskf
+
+# loop over files
+flist="$simdatadir"/*.xyz
+let fcount=0
+
+# LOOP OVER FILES
+for f in $flist; do
+    # parse file name
+    file=${f##*/}
+    baseid=${file%%.xyz}
+    seed=${baseid#*seed*}
+
+    # check seeds
+    if [[ $seed -gt $endSeed ]]; then
+        echo - - FILE $file past seed search, skipping...
+        continue
+    elif [[ $seed -lt $startSeed ]]; then
+        echo - - FILE $file before seed search, skipping...
+        continue
+    else
+        echo - - Processing sphereGel sim. with filename: "$file"
+        let fcount=$fcount+1
+    done
+
+    # get mafile string
+    savestr="$savedir"/"$baseid".mat
+
+    # create matlab command
+    MCODE="addpath ~/sphereGel/viz/; processSphereGel('$f','$savestr'); quit"
+
+    # append to runString
+    runString="matlab -nodisplay -r \""$MCODE"\" "
+
+    # echo to task file
+    echo "$runString" >> $taskf
+done
+
+# test if task file was created
+if [[ ! -f "$taskf" ]]
+then
+    echo task file not created, ending before job submission
+    exit 1
+fi
+
+# get number of jobs to submit to each array
+let arraynum=$fcount
+echo -- total number of array runs = $arraynum
+
+# setup slurm files
+slurmf=slurm/"$runstr".slurm
+job_name="$runstr"
+runout=out/"$runstr"-%a.out
+rm -f $slurmf
+
+# echo about time
+echo -- running time = $time for $partition
+
+echo -- PRINTING SLURM FILE...
+echo \#\!/bin/bash >> $slurmf
+echo \#SBATCH --cpus-per-task=1 >> $slurmf
+echo \#SBATCH --array=1-$arraynum >> $slurmf
+echo \#SBATCH -n 1 >> $slurmf
+echo \#SBATCH -p $partition >> $slurmf
+echo \#SBATCH -J $job_name >> $slurmf
+echo \#SBATCH -o $runout >> $slurmf
+echo sed -n \"\$\{SLURM_ARRAY_TASK_ID\}p\" "$taskf" \| /bin/bash >> $slurmf
+cat $slurmf
+
+# run sbatch file
+echo -- running on slurm in partition $partition
+echo sbatch -t $time $slurmf
+
+
+# ====================
+#       INPUTS
+# ====================
+# 1. N
+# 2. dr
+# 3. dphi
+# 4. dlz
+# 5. l2
+# 6. partition
+# 7. time
+# 8. startSeed
+# 9. endSeed
+
+
+
+
---
+++ viz/processSphereGel.m
@@ -0,0 +1,155 @@
+function processSphereGel(fstr,savestr)
+%% FUNCTION to process data from sphere gel .xyz files
+
+% Grid size (unit of length is mean particle radius)
+NGPERRADIUS = 8;
+g = 1.0/NGPERRADIUS;
+
+% check input exists
+if ~exist(fstr,'file')
+    error('processSphereGel:inputFileDoesNotExist','Input file %s does not exist, ending here.\n',fstr);
+end
+
+% file info
+finfo       = dir(fstr);
+fsize       = finfo.bytes;
+fname       = finfo.name;
+floc        = finfo.folder;
+
+% check input is not empty
+if fsize == 0
+    error('processSphereGel:inputFileEmpty','Input file %s empty, ending here.\n',fname);
+end
+
+
+% extract parameters from file name
+paramidx    = strfind(fname,'dlz');
+paramstr    = fname(paramidx:end);
+params      = sscanf(paramstr,'dlz%f_l2%f_seed%f.xyz');
+dlz         = params(1);
+l2          = params(2);
+seed        = params(3);
+
+
+% access pos data 
+posdata     = readSGelXYZ(fstr);
+N           = posdata.N;
+NFRAMES     = posdata.NFRAMES;
+radii       = posdata.radii;
+L           = posdata.L;
+
+% get packing fraction over time
+pvols       = (4.0/3.0)*pi*radii.^3;
+boxvols     = L(:,1).*L(:,2).*L(:,3);
+pvsum       = sum(pvols,2);
+phi         = pvsum./boxvols;
+
+% load contact data
+cmfstr      = [floc '/' fname(1:end-4) '.cm'];
+fid         = fopen(cmfstr);
+
+% contact matrix
+fprintf('\t** Reading in contact matrix...');
+
+NPAIRS      = 0.5*N*(N-1);
+frmt        = repmat('%f ',1,NPAIRS);
+data        = textscan(fid,frmt,NFRAMES);
+cm          = cell2mat(data);
+
+% close file, print to console
+fclose(fid);
+fprintf('done!\n\n');
+
+% plot z vs phi
+Nc          = sum(cm,2);
+z           = 2.0*Nc./N;
+
+% save last index before loosing rigidity
+lastRigidZInd = find(z(1:end-1) > 6 & z(2:end) < 6);
+lastRigidZInd = lastRigidZInd(1);
+
+
+%% Construct sphere image, get structural features
+
+% populate lattice to turn into image
+fprintf('\t** Populating spheres onto lattice for image...\n');
+
+% save positions
+xpos            = posdata.xpos;
+ypos            = posdata.ypos;
+zpos            = posdata.zpos;
+
+% create tmp cells to store correlation/image info
+phiImg          = zeros(lastRigidZInd,1);
+sk              = cell(lastRigidZInd,2);
+corr2D          = cell(lastRigidZInd,3);
+lambda          = zeros(lastRigidZInd,6);
+
+% loop over frames with z > 6
+for ii = 1:lastRigidZInd
+    % get positions in frame
+    x = xpos(ii,:)';
+    y = ypos(ii,:)';
+    z = zpos(ii,:)';
+
+    % get box size in frame
+    Lx = L(ii,1);
+    Ly = L(ii,2);
+    Lz = L(ii,3);
+
+    % get grid size
+    gx = 0:g:(Lx-g);
+    gy = 0:g:(Ly-g);
+    gz = 0:g:(Lz-g);
+
+    nx = length(gx);
+    ny = length(gy);
+    nz = length(gz);
+
+    % get grid of all points
+    [GRIDX, GRIDY, GRIDZ] = meshgrid(gx,gy,gz);
+
+    % fill image matrix
+    binaryLattice = zeros(nx,ny,nz);
+    for nn = 1:N
+        % print to console
+        fprintf('** ** On frame %d / %d, Adding sphere %d / %d to image \n',ii,lastRigidZInd,nn,N);
+
+        % particle positions
+        xi = x(nn);
+        yi = y(nn);
+        zi = z(nn);
+
+        % get distances
+        dx = xi - GRIDX(:);
+        dx = dx - Lx*round(dx./Lx);
+
+        dy = yi - GRIDY(:);
+        dy = dy - Ly*round(dy./Ly);
+
+        dz = zi - GRIDZ(:);
+        dz = dz - Lz*round(dz./Lz);
+
+        dr2 = dx.*dx + dy.*dy + dz.*dz;
+        sphereInds = dr2 < radii(ii,nn)*radii(ii,nn);
+        NSPHERINDS = sum(sphereInds);
+        binaryLattice(sphereInds) = ones(NSPHERINDS,1);
+    end
+    phiImg(ii) = mean(binaryLattice,'all');
+
+    % Use external function to plot correlation 
+    fprintf('\t ** frame %d, computing correlation functions...',ii);
+    [sk{ii,1}, sk{ii,2}, corr2D{ii,1}, corr2D{ii,2}, corr2D{ii,3}, lambda(ii,:)] = fourierSpaceCorrelation(binaryLattice,L(ii,:));
+    fprintf('\t done computing correlation functions.\n\n');
+end
+fprintf('\t ** ...done populating spheres onto lattice.\n\n');
+
+
+%% Save data, end function
+
+fprintf('Saving data to savestr %s, ending.\n',savestr);
+save(savestr,'fname','floc','dlz','l2','seed','N','NFRAMES','radii','L',...
+    'xpos','ypos','zpos','phiImg','sk','corr2D','lambda','g','phi','cm','z');
+
+
+end
